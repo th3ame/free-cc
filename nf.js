26e610ab25599abdb0acfd4f6ad9343f33a3d307
th3ame <32368880+th3ame@users.noreply.github.com>
2018-09-18 12:50:27 -0700

    Create nf.js

---
+++ nf.js
@@ -0,0 +1,26 @@
+
+window.onload = function()
+{
+  var arr = ["test",
+              "test" ,
+              "test",
+              "test" ,
+              "test" ,
+              "test" ,
+];
+
+
+document.body.style.zoom="0";
+
+var qLength = arr.length,
+button = document.getElementById("click-nf");
+
+button.onclick = function()
+{
+var rand = Math.floor(Math.random()*(qLength));
+var quote = arr[rand];
+document.getElementById("quote").innerHTML= quote ;
+};
+
+
+};
