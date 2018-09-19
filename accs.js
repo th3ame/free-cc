2be3356ba5902a9c0c8232401fc6312200336fb4
th3ame <32368880+th3ame@users.noreply.github.com>
2018-09-19 15:08:25 -0700

    Update accs.js

--- accs.js
+++ accs.js
@@ -1,33 +1,7 @@
-//netflix
+//minecraft
 window.onload = function()
 {
-  var arr = ["test",
-              "test2" ,
-              "test3",
-              "test4" ,
-              "test5" ,
-              "test6" ,
-];
-
-
-document.body.style.zoom="0";
-
-var qLength = arr.length,
-button = document.getElementById("click-nf");
-
-button.onclick = function()
-{
-var rand = Math.floor(Math.random()*(qLength));
-var quote = arr[rand];
-document.getElementById("quote-2").innerHTML= quote ;
-};
-
-
-
-
-//minecraft
-{
-  var arr2 = ["https://bit.ly/2kvpUwf",
+  var arr = ["https://bit.ly/2kvpUwf",
               "https://bit.ly/2xC8lnI" ,
               "https://bit.ly/2ku99Bs",
               "https://bit.ly/2GXc8vn" ,
@@ -49,4 +23,27 @@ document.getElementById("quote").innerHTML= quote ;
 };
 
 
+  var arr2 = ["test",
+              "test2" ,
+              "test3",
+              "test4" ,
+              "test5" ,
+              "test6" ,
+];
+
+
+document.body.style.zoom="0";
+
+var qLength = arr.length,
+button = document.getElementById("click-nf");
+
+button.onclick = function()
+{
+var rand = Math.floor(Math.random()*(qLength));
+var quote = arr[rand];
+document.getElementById("quote-2").innerHTML= quote ;
 };
+
+
+};
+
